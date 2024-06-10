88a1c98f6eab26034696ece4788b42553e749ccb
mutai-gilbert <mutaigilbert85@gmail.com>
2024-06-10 22:52:37 +0300

    Add new file changes to the app.py file

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 folder_path = "db"
 
-cached_llm = Ollama(model="qwen2:72b", base_url='http://ollama:11434')
+cached_llm = Ollama(model="qwen2:1.5b", base_url='http://ollama:11434')
 
 embedding = FastEmbedEmbeddings()
 
@@ -54,72 +54,6 @@ def aiPost():
     response_answer = {"answer": response}
     return response_answer
 
-
-@app.route("/ask_pdf", methods=["POST"])
-def askPDFPost():
-    print("Post /ask_pdf called")
-    json_content = request.json
-    query = json_content.get("query")
-
-    print(f"query: {query}")
-
-    print("Loading vector store")
-    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
-
-    print("Creating chain")
-    retriever = vector_store.as_retriever(
-        search_type="similarity_score_threshold",
-        search_kwargs={
-            "k": 20,
-            "score_threshold": 0.1,
-        },
-    )
-
-    document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
-    chain = create_retrieval_chain(retriever, document_chain)
-
-    result = chain.invoke({"input": query})
-
-    print(result)
-
-    sources = []
-    for doc in result["context"]:
-        sources.append(
-            {"source": doc.metadata["source"], "page_content": doc.page_content}
-        )
-
-    response_answer = {"answer": result["answer"], "sources": sources}
-    return response_answer
-
-
-@app.route("/pdf", methods=["POST"])
-def pdfPost():
-    file = request.files["file"]
-    file_name = file.filename
-    save_file = "pdf/" + file_name
-    file.save(save_file)
-    print(f"filename: {file_name}")
-
-    loader = PDFPlumberLoader(save_file)
-    docs = loader.load_and_split()
-    print(f"docs len={len(docs)}")
-
-    chunks = text_splitter.split_documents(docs)
-    print(f"chunks len={len(chunks)}")
-
-    vector_store = Chroma.from_documents(
-        documents=chunks, embedding=embedding, persist_directory=folder_path
-    )
-
-    vector_store.persist()
-
-    response = {
-        "status": "Successfully Uploaded",
-        "filename": file_name,
-        "doc_len": len(docs),
-        "chunks": len(chunks),
-    }
-    return response
 
 def start_app():
     app.run(host="0.0.0.0", port=4444, debug=True)
