92e0bcfa5d2b8f5c10e4e98d38906d7ae165263b
mutai-gilbert <mutaigilbert85@gmail.com>
2024-06-14 23:50:08 +0300

    Renamed the files according to the ollama_models name

---
+++ qwen2/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
+
+app = Flask(__name__)
+
+folder_path = "db"
+
+cached_llm = Ollama(model="qwen2:1.5b", base_url='http://ollama:11434')
+
+embedding = FastEmbedEmbeddings()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
+
+raw_prompt = PromptTemplate.from_template(
+    """
+    <s>[INST] You are a technical assistant good at searching docuemnts. If you do not have an answer from the provided information say so. [/INST] </s>
+    [INST] {input}
+           Context: {context}
+           Answer:
+    [/INST]
+"""
+)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "this is the Qwen2 72b api server "
+
+# ping pong test
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "pong"
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+    response = cached_llm.invoke(query)
+
+    print(response)
+
+    response_answer = {"answer": response}
+    return response_answer
+
+
+def start_app():
+    app.run(host="0.0.0.0", port=4444, debug=True)
+
+if __name__ == "__main__":
+    start_app()
